c3f05ef9add143cdd1d8c509fdc18bdd21fadf61
GauravSamanta <gauravsamanta143@gmail.com>
2024-07-19 20:03:55 +0530

    fixing another cors error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./routes/HomePage";
+import LoginPage from "./routes/LoginPage";
+import RegisterPage from "./routes/RegisterPage";
 
 axios.defaults.baseURL = "https://url-jar-home.vercel.app";
 axios.defaults.withCredentials = true;
@@ -9,6 +11,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/register" element={<RegisterPage />}></Route>
     </Routes>
   );
 }
